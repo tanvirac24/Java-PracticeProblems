9c67510a7a05a865c0d93126fd33fbfbd0e73ba1
tanvirac24 <tanvirac024@gmail.com>
2025-02-17 23:59:27 +0600

    practice

---
+++ 1.js
@@ -0,0 +1,17 @@
+let n=90
+if(n>80 && n<100){
+    console.log("A+")
+}
+else if(n>=71 && n<=79){
+    console.log("A")
+}
+else if(n>=61 && n<=69){
+    console.log("A-")
+}
+else if(n>=41 && n<=59){
+    console.log("B")
+}
+else if(n>=40){
+    console.log("C")
+}
+else{console.log("F")}
